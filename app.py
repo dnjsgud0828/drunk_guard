3fcb05f921ea1e437c0176122efe1ad66bf91ea7
happySealion <dnjsgud0828@gmail.com>
2025-04-22 12:58:43 +0900

    Add WebCam

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
-from flask import Flask, render_template
-from dotenv import load_dotenv
+from flask import Flask, render_template, Response, request, redirect
+from camera import VideoCamera
+# from db import save_prediction, get_drunk_users
 import os
 
-#load .env
-load_dotenv
-
 PORT = os.environ.get('PORT')
-KAKAO_ID = os.environ.get('KAKAO_ID')
 
 app = Flask(__name__)
+camera = VideoCamera()
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/elements')
-def elements():
-    return render_template('elements.html')
+@app.route('/detect')
+def detect():
+    return render_template('detect.html')
 
-@app.route('/generic')
-def generic():
-    return render_template('generic.html')
+@app.route('/video_feed')
+def video_feed():
+    return Response(camera.generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/signUp')
-def signUp():
-    return render_template('signUp.html')
+@app.route('/log')
+def log():
+    # drunk_users = get_drunk_users()
+    # return render_template('log.html', users=drunk_users)
+    return render_template('log.html')
 
 if __name__ == '__main__':
     print(f"{PORT}번 포트에서 대기 중")
